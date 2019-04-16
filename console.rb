9a8a3bada53c3bf5d474060d10b9f4c69028c605
malkyx <malky.burns@gmail.com>
2019-04-16 15:55:08 +0100

    add find method to property class

--- console.rb
+++ console.rb
@@ -3,4 +3,10 @@ require_relative('models/property')
 
 property = Property.new({'value' => '131000', 'number_bedrooms' => '3', 'year_built' => '2000', 'buy_let_status' => 'buy'})
 
-property.save()
+# property.save()
+# property.save()
+# property.delete()
+# property.value = 231000
+# property.update()
+
+p property.find(8)
--- models/property.rb
+++ models/property.rb
@@ -24,11 +24,33 @@ class Property
   db.close()
   end
 
+  def delete
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "DELETE FROM property_tracker WHERE id = $1;"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+    db.close()
+  end
 
+  def update
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "UPDATE property_tracker SET (value, number_bedrooms, year_built, buy_let_status) = ($1, $2, $3, $4) WHERE id = $5;"
+    values = [@value, @number_bedrooms, @year_built, @buy_let_status, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
+  end
 
-
-
-
-
+  def find(id)
+    db = PG.connect({dbname: 'property_tracker', host: 'localhost'})
+    sql = "SELECT * FROM property_tracker WHERE id = $1;"
+    values = [id]
+    db.prepare("find", sql)
+    property_hash = db.exec_prepared("find", values)
+    found_property = Property.new(property_hash[0])
+    db.close()
+    return found_property
+  end
 
 end
